612b7e2c4664e5501b1f8abae94baf0d10789975
rasmuseeg <rasmuseeg@outlook.com>
2019-11-07 17:21:01 +0100

    Renamed project folders

---
+++ src/Application/Common/Interfaces/IUserManager.cs
@@ -0,0 +1,16 @@
+﻿using Doctrina.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doctrina.Application.Common.Interfaces
+{
+    public interface IUserManager
+    {
+        Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password);
+
+        Task<Result> DeleteUserAsync(string userId);
+    }
+}
