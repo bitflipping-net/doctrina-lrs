96e3e804824bc7a9c9c6c1a87e9b8fff3d4a9f81
rasmuseeg <rasmuseeg@outlook.com>
2019-11-24 18:32:33 +0100

    Added attachments from db to result

--- src/Application/Infrastructure/Automapper/Mappings/TypeConverters/StatementTypeConverter.cs
+++ src/Application/Infrastructure/Automapper/Mappings/TypeConverters/StatementTypeConverter.cs
@@ -11,6 +11,10 @@ namespace Doctrina.Application.Infrastructure.Automapper.Mappings.TypeConverters
             // FullStatement is the fastest way, allows us to not innerjoin other tables for the final result.
             // But it does require fullStatement to be update to date.
             var stmt = new Statement(source.FullStatement);
+            if(stmt.Attachments != null && stmt.Attachments.Count > 0)
+            {
+                context.Mapper.Map(source.Attachments, stmt.Attachments);
+            }
             return stmt;
         }
     }
